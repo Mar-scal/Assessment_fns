5fccb72bc17a5a2b333eadb317125aaf525bf7f1
freyakeyser <freyakeyser@gmail.com>
2019-06-04 16:47:50 -0300

    don't need all this after all, just predict.gam!

--- Other_functions/gam_post_simulate.R
+++ Other_functions/gam_post_simulate.R
@@ -29,8 +29,9 @@ gam_post_simulate <- function(gamobj, moddat){
   # ci <- quantile(opt, c(.025,.975)) ## get 95% CI
   # ci
 
-  fits <- cbind(as.data.frame(predict(gamobj, se.fit=T)), 
+  fits <- cbind(as.data.frame(predict(gamobj, se.fit=T, unconditional=T)), # unconditional=T based on advice from predict.gam/plot.gam (sewithMean option)
                           gamobj$model) 
+  
   names(fits) <- c("fit", "se.fit", "y", "x")
   
   if(gamobj$family$link == "logit") ilink <- function(x) inv.logit(x)
@@ -49,81 +50,82 @@ gam_post_simulate <- function(gamobj, moddat){
   print(modplot)
   #dev.off()
   
-  
-  # from https://www.fromthebottomoftheheap.net/2016/12/15/simultaneous-interval-revisited/
-  rmvn <- function(n, mu, sig) { ## MVN random deviates
-    L <- mroot(sig)
-    m <- ncol(L)
-    t(mu + L %*% matrix(rnorm(m*n), m, n))
-  }
-  #Next we extract a few things that we need from the fitted GAM
-  
-  Vb <- vcov(gamobj)
-  se.fit <- fits$se.fit
-  # The first is the Bayesian covariance matrix of the model coefficients, Vb. This Vb is conditional upon the smoothing parameter(s). If you want a version that adjusts for the smoothing parameters being estimated rather than known values, add unconditional = TRUE to the vcov() call. Second, we define our grid of x values over which we want a confidence band. Then we generate predictions and standard errors from the model for the grid of values. The last line just extracts out the standard errors of the fitted values for use later.
-  
-  # Now we are ready to generate simulations of the maximum absolute standardized deviation of the fitted model from the true model. We set the pseudo-random seed to make the results reproducible and specify the number of simulations to generate.
-  
-  set.seed(42)
-  N <- 10000
-  
-  BUdiff <- rmvn(N, mu = rep(0, nrow(Vb)), sig = Vb)
-  
-  Cg <- predict(gamobj, gamobj$model, type = "lpmatrix")
-  simDev <- Cg %*% t(BUdiff)
-  # The first line evaluates the basis function at g and the second line computes the deviations between the fitted and true parameters. Then we find the absolute values of the standardized deviations from the true model. Here we do this in a single step for all simulations using sweep()
-  
-  absDev <- abs(sweep(simDev, 1, se.fit, FUN = "/"))
-  
-  masd <- apply(absDev, 2L, max)
-  
-  # The last step is to find the critical value used to scale the standard errors to yield the simultaneous interval; here we calculate the critical value for a 95% simultaneous confidence interval/band
-  crit <- quantile(masd, prob = 0.95, type = 8)
-  #The critical value estimated above is 2.816688. 
-  # Now that we have the critical value, we can calculate the simultaneous confidence interval. In the code block below I first add the grid of values (newd) to the fitted values and standard errors at those new values and then augment this with upper and lower limits for a 95% simultaneous confidence interval (uprS and lwrS), as well as the usual 95% point-wise intervals for comparison (uprP and lwrP). Then I plot the two intervals:
-  
-  pred <- transform(cbind(data.frame(fits), gamobj$model),
-                    uprP = inv.logit(fit + (2 * se.fit)),# pointwise (narrower, same as before)
-                    lwrP = inv.logit(fit - (2 * se.fit)),#pointwise 
-                    uprS = inv.logit(fit + (crit * se.fit)), # simultaneous (wider)
-                    lwrS = inv.logit(fit - (crit * se.fit))) # simultaneous
-  
-  sims <- rmvn(N, mu = coef(gamobj), sig = Vb)
-  fits2 <- ilink(Cg %*% t(sims))
-  nrnd <- 30
-  rnd <- sample(N, nrnd)
-  stackFits <- stack(as.data.frame(fits2[, rnd]))
-  stackFits <- transform(stackFits, x=unique(fits$x))
-  stackFits <- unique(stackFits[, c("values", "x")])
-  stackFits$ind <- rep(1:(nrow(stackFits)/length(unique(fits$x))), each=length(unique(fits$x)))
-  
-  modplot_CI <- modplot + geom_ribbon(data=pred, aes(x, ymin = lwrS, ymax = uprS), alpha = 0.2, fill = "red") +
-          geom_ribbon(data=pred, aes(x, ymin = lwrP, ymax = uprP), alpha = 0.2, fill = "red") +
-          geom_path(data = stackFits, mapping = aes(y = values, x=x, group=ind),
-                    alpha = 0.4, colour = "grey20")
-  print(modplot_CI)
-  
-  inCI <- function(x, upr, lwr) {
-    all(x >= lwr & x <= upr)
-  }
-  
-  fitsInPCI <- apply(fits2, 2L, inCI, upr = pred$uprP, lwr = pred$lwrP)
-  fitsInSCI <- apply(fits2, 2L, inCI, upr = pred$uprS, lwr = pred$lwrS)
-  
-  checkSimCI <- sum(fitsInSCI) / length(fitsInSCI)
-  checkPntWiseCI = sum(fitsInPCI) / length(fitsInPCI)
-  
-  print(checkSimCI)   # Point-wise
-  print(checkPntWiseCI)  # Simultaneous
-  
-  modplot_final <- ggplot() + geom_point(data=fits, aes(x, y)) +
-    geom_line(data=fits, aes(x, ilink(fit)), colour="red") +
-    theme_bw() +
-    theme(panel.grid=element_blank()) +
-    ggtitle(gamobj$call) +
-    geom_line(data=pred, aes(x, y = lwrS), linetype="dashed", colour = "red") +
-    geom_line(data=pred, aes(x, y = uprS), linetype="dashed", colour = "red")
-  print(modplot_final)
-  
-  return(list(modplot=modplot, modplot_CI=modplot_CI, modplot_final=modplot_final, checkSimCI = checkSimCI, checkPntWiseCI=checkPntWiseCI))
+  # # from https://www.fromthebottomoftheheap.net/2016/12/15/simultaneous-interval-revisited/
+  # rmvn <- function(n, mu, sig) { ## MVN random deviates
+  #   L <- mroot(sig)
+  #   m <- ncol(L)
+  #   t(mu + L %*% matrix(rnorm(m*n), m, n))
+  # }
+  # #Next we extract a few things that we need from the fitted GAM
+  # 
+  # Vb <- vcov(gamobj)
+  # se.fit <- fits$se.fit
+  # # The first is the Bayesian covariance matrix of the model coefficients, Vb. This Vb is conditional upon the smoothing parameter(s). If you want a version that adjusts for the smoothing parameters being estimated rather than known values, add unconditional = TRUE to the vcov() call. Second, we define our grid of x values over which we want a confidence band. Then we generate predictions and standard errors from the model for the grid of values. The last line just extracts out the standard errors of the fitted values for use later.
+  # 
+  # # Now we are ready to generate simulations of the maximum absolute standardized deviation of the fitted model from the true model. We set the pseudo-random seed to make the results reproducible and specify the number of simulations to generate.
+  # 
+  # set.seed(42)
+  # N <- 100
+  # 
+  # BUdiff <- rmvn(N, mu = rep(0, nrow(Vb)), sig = Vb)
+  # 
+  # Cg <- predict(gamobj, gamobj$model, type = "lpmatrix")
+  # simDev <- Cg %*% t(BUdiff)
+  # # The first line evaluates the basis function at g and the second line computes the deviations between the fitted and true parameters. Then we find the absolute values of the standardized deviations from the true model. Here we do this in a single step for all simulations using sweep()
+  # 
+  # absDev <- abs(sweep(simDev, 1, se.fit, FUN = "/"))
+  # 
+  # masd <- apply(absDev, 2L, max)
+  # 
+  # # The last step is to find the critical value used to scale the standard errors to yield the simultaneous interval; here we calculate the critical value for a 95% simultaneous confidence interval/band
+  # crit <- quantile(masd, prob = 0.95, type = 8)
+  # #The critical value estimated above is 2.816688. 
+  # # Now that we have the critical value, we can calculate the simultaneous confidence interval. In the code block below I first add the grid of values (newd) to the fitted values and standard errors at those new values and then augment this with upper and lower limits for
+  # # a 95% simultaneous confidence interval (uprS and lwrS), as well as the usual 95% point-wise intervals for comparison (uprP and lwrP). Then I plot the two intervals:
+  # 
+  # pred <- transform(cbind(data.frame(fits), gamobj$model),
+  #                   uprP = inv.logit(fit + (2 * se.fit)),# pointwise (narrower, same as before)
+  #                   lwrP = inv.logit(fit - (2 * se.fit)),#pointwise 
+  #                   uprS = inv.logit(fit + (crit * se.fit)), # simultaneous (wider)
+  #                   lwrS = inv.logit(fit - (crit * se.fit))) # simultaneous
+  # 
+  # sims <- rmvn(N, mu = coef(gamobj), sig = Vb)
+  # fits2 <- ilink(Cg %*% t(sims))
+  # nrnd <- 30
+  # rnd <- sample(N, nrnd)
+  # stackFits <- stack(as.data.frame(fits2[, rnd]))
+  # stackFits <- transform(stackFits, x=unique(fits$x))
+  # stackFits <- unique(stackFits[, c("values", "x")])
+  # stackFits$ind <- rep(1:(nrow(stackFits)/length(unique(fits$x))), each=length(unique(fits$x)))
+  # 
+  # modplot_CI <- modplot + geom_ribbon(data=pred, aes(x, ymin = lwrS, ymax = uprS), alpha = 0.2, fill = "red") +
+  #         geom_ribbon(data=pred, aes(x, ymin = lwrP, ymax = uprP), alpha = 0.2, fill = "red") +
+  #         geom_path(data = stackFits, mapping = aes(y = values, x=x, group=ind),
+  #                   alpha = 0.4, colour = "grey20")
+  # print(modplot_CI)
+  # 
+  # inCI <- function(x, upr, lwr) {
+  #   all(x >= lwr & x <= upr)
+  # }
+  # 
+  # fitsInPCI <- apply(fits2, 2L, inCI, upr = pred$uprP, lwr = pred$lwrP)
+  # fitsInSCI <- apply(fits2, 2L, inCI, upr = pred$uprS, lwr = pred$lwrS)
+  # 
+  # checkSimCI <- sum(fitsInSCI) / length(fitsInSCI)
+  # checkPntWiseCI = sum(fitsInPCI) / length(fitsInPCI)
+  # 
+  # print(checkSimCI)   # Simultaneous
+  # print(checkPntWiseCI)  # Point-wise
+  # 
+  # modplot_final <- ggplot() + geom_point(data=fits, aes(x, y)) +
+  #   geom_line(data=fits, aes(x, ilink(fit)), colour="red") +
+  #   theme_bw() +
+  #   theme(panel.grid=element_blank()) +
+  #   ggtitle(gamobj$call) +
+  #   geom_line(data=pred, aes(x, y = lwrS), linetype="dashed", colour = "red") +
+  #   geom_line(data=pred, aes(x, y = uprS), linetype="dashed", colour = "red")
+  # print(modplot_final)
+  # 
+  return(list(modplot=modplot#, modplot_CI=modplot_CI, modplot_final=modplot_final, checkSimCI = checkSimCI, checkPntWiseCI=checkPntWiseCI
+              ))
 }
