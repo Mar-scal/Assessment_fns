f5a2f796fa67c72f947bc38990ac393d96c87f6b
David Keith <dave.keith@outlook.com>
2019-09-25 11:28:12 -0300

    Restructuring Folders
    Pulling out the scripts that are not core assessment script, putting those in a new "supporting_tasks" repo

--- RUNME_Survey_OSAC_Model/tow_tracks_script.r
+++
@@ -1,8 +0,0 @@
-source("D:/R/Assessment_fns/Survey_and_OSAC/Plotting_tow_tracks.r")
-
-# For the spring run this, note that this doesn't run BBs.
-res <- tow.track.plots(bk = 'spring', extras = T, export=T,fig="png",compare=T,labels=T,year=2018)
-
-# For the summer this should do the trick
-res <- tow.track.plots(bk = "summer", extras = T, export=T,fig="png",compare=T,labels=T,year=2018)
-
