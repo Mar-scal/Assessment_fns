4eaf202f89ea5208b54326d9e064bb415138368d
Freya Keyser <freyakeyser@gmail.com>
2020-08-11 11:10:47 -0300

    clean versions of runme's

---
+++ RUNME/detailed_tow_data_for_SPANS.R
@@ -0,0 +1,134 @@
+# For Detailed Tow Data (for SPANS, usually right after Industry report)
+year<- 2019
+DR <- "DR2019_10"
+direct <- "Y:/Offshore scallop/Assessment/"
+load(paste0(direct, "/Data/Survey_data/", year, "/Survey_summary_output/testing_results_SCALOFF_LE10.Rdata"))
+
+banks <- names(survey.obj)
+
+# cruises <- c("GB", "BB", "SAB", "MID", "BAN", "GER")
+cruises <- c("GBa", "GBb")
+
+
+# read in this function
+detailed.tow.data <- function(year=2018, DR="DR2018_03", banks=banks, cruises=cruises, un.ID=un.ID, pwd.ID=pwd.ID){
+
+  require(ROracle)
+  
+  chan <-dbConnect(dbDriver("Oracle"),username=un.ID, password=pwd.ID,db.con)
+  
+  sh.live <- dbGetQuery(chan, paste0("SELECT CRUISE, YEAR, SURVEY_NAME, MGT_AREA_CD, SPECIES_ID, TOW_NO, TOW_TYPE_ID, DEPTH_F, TOW_DATE, START_LAT, START_LON, END_LAT, END_LON, 
+                                   round(bin_0, 2) bin_0,round(bin_5, 2) bin_5,round(bin_10, 2) bin_10,round(bin_15, 2) bin_15,round(bin_20, 2) bin_20,round(bin_25, 2) bin_25,round(bin_30, 2) bin_30,round(bin_35, 2) bin_35,round(bin_40, 2) bin_40,
+                                   round(bin_45, 2) bin_45,round(bin_50, 2) bin_50,round(bin_55, 2) bin_55,round(bin_60, 2) bin_60,round(bin_65, 2) bin_65,round(bin_70, 2) bin_70,round(bin_75, 2) bin_75,round(bin_80, 2) bin_80,round(bin_85, 2) bin_85,
+                                   round(bin_90, 2) bin_90,round(bin_95, 2) bin_95,round(bin_100, 2) bin_100,round(bin_105, 2) bin_105,round(bin_110, 2) bin_110,round(bin_115, 2) bin_115,round(bin_120, 2) bin_120,round(bin_125, 2) bin_125,round(bin_130, 2) bin_130,
+                                   round(bin_135, 2) bin_135,round(bin_140, 2) bin_140,round(bin_145, 2) bin_145,round(bin_150, 2) bin_150,round(bin_155, 2) bin_155,round(bin_160, 2) bin_160,round(bin_165, 2) bin_165,round(bin_170, 2) bin_170,round(bin_175, 2) bin_175,
+                                   round(bin_180, 2) bin_180,round(bin_185, 2) bin_185,round(bin_190, 2) bin_190,round(bin_195,2) bin_195, 'live' state
+                                   FROM SCALOFF.OSLIVERES_SS_VW  
+                                   WHERE YEAR = ", year))
+  
+  sh.dead <- dbGetQuery(chan, paste0("SELECT CRUISE, YEAR, SURVEY_NAME, MGT_AREA_CD, SPECIES_ID, TOW_NO, TOW_TYPE_ID, DEPTH_F, TOW_DATE, START_LAT, START_LON, END_LAT, END_LON, 
+                                   round(bin_0, 2) bin_0,round(bin_5, 2) bin_5,round(bin_10, 2) bin_10,round(bin_15, 2) bin_15,round(bin_20, 2) bin_20,round(bin_25, 2) bin_25,round(bin_30, 2) bin_30,round(bin_35, 2) bin_35,round(bin_40, 2) bin_40,
+                                   round(bin_45, 2) bin_45,round(bin_50, 2) bin_50,round(bin_55, 2) bin_55,round(bin_60, 2) bin_60,round(bin_65, 2) bin_65,round(bin_70, 2) bin_70,round(bin_75, 2) bin_75,round(bin_80, 2) bin_80,round(bin_85, 2) bin_85,
+                                   round(bin_90, 2) bin_90,round(bin_95, 2) bin_95,round(bin_100, 2) bin_100,round(bin_105, 2) bin_105,round(bin_110, 2) bin_110,round(bin_115, 2) bin_115,round(bin_120, 2) bin_120,round(bin_125, 2) bin_125,round(bin_130, 2) bin_130,
+                                   round(bin_135, 2) bin_135,round(bin_140, 2) bin_140,round(bin_145, 2) bin_145,round(bin_150, 2) bin_150,round(bin_155, 2) bin_155,round(bin_160, 2) bin_160,round(bin_165, 2) bin_165,round(bin_170, 2) bin_170,round(bin_175, 2) bin_175,
+                                   round(bin_180, 2) bin_180,round(bin_185, 2) bin_185,round(bin_190, 2) bin_190,round(bin_195,2) bin_195, 'dead' state
+                                   FROM SCALOFF.OSDEADRES_SS_VW  
+                                   WHERE YEAR = ", year))
+  
+  mw.sql <- dbGetQuery(chan, paste0("SELECT CRUISE, YEAR, SURVEY_NAME, MGT_AREA_CD, SPECIES_ID, TOW_NO, DEPTH_F, TOW_DATE, START_LAT, START_LON, END_LAT, END_LON, SCALLOP_NUM, SHELL_HEIGHT, WET_MEAT_WGT
+                              FROM SCALOFF.OSSAMPLES_SS_VW
+                              WHERE YEAR = ", year))
+  
+  if("BanIce" %in% banks){
+    sh.live.ice <- dbGetQuery(chan, paste0("SELECT CRUISE, YEAR, SURVEY_NAME, MGT_AREA_CD, SPECIES_ID, TOW_NO, TOW_TYPE_ID, DEPTH_F, TOW_DATE, START_LAT, START_LON, END_LAT, END_LON, 
+                                   round(bin_0, 2) bin_0,round(bin_5, 2) bin_5,round(bin_10, 2) bin_10,round(bin_15, 2) bin_15,round(bin_20, 2) bin_20,round(bin_25, 2) bin_25,round(bin_30, 2) bin_30,round(bin_35, 2) bin_35,round(bin_40, 2) bin_40,
+                                   round(bin_45, 2) bin_45,round(bin_50, 2) bin_50,round(bin_55, 2) bin_55,round(bin_60, 2) bin_60,round(bin_65, 2) bin_65,round(bin_70, 2) bin_70,round(bin_75, 2) bin_75,round(bin_80, 2) bin_80,round(bin_85, 2) bin_85,
+                                   round(bin_90, 2) bin_90,round(bin_95, 2) bin_95,round(bin_100, 2) bin_100,round(bin_105, 2) bin_105,round(bin_110, 2) bin_110,round(bin_115, 2) bin_115,round(bin_120, 2) bin_120,round(bin_125, 2) bin_125,round(bin_130, 2) bin_130,
+                                   round(bin_135, 2) bin_135,round(bin_140, 2) bin_140,round(bin_145, 2) bin_145,round(bin_150, 2) bin_150,round(bin_155, 2) bin_155,round(bin_160, 2) bin_160,round(bin_165, 2) bin_165,round(bin_170, 2) bin_170,round(bin_175, 2) bin_175,
+                                   round(bin_180, 2) bin_180,round(bin_185, 2) bin_185,round(bin_190, 2) bin_190,round(bin_195,2) bin_195, 'live' state
+                                   FROM SCALOFF.OSLIVERES_ICE_VW  
+                                   WHERE YEAR = ", year))
+    sh.live <-rbind(sh.live, sh.live.ice)
+    sh.dead.ice <- dbGetQuery(chan, paste0("SELECT CRUISE, YEAR, SURVEY_NAME, MGT_AREA_CD,  SPECIES_ID, TOW_NO, TOW_TYPE_ID, DEPTH_F, TOW_DATE, START_LAT, START_LON, END_LAT, END_LON, 
+                                   round(bin_0, 2) bin_0,round(bin_5, 2) bin_5,round(bin_10, 2) bin_10,round(bin_15, 2) bin_15,round(bin_20, 2) bin_20,round(bin_25, 2) bin_25,round(bin_30, 2) bin_30,round(bin_35, 2) bin_35,round(bin_40, 2) bin_40,
+                                   round(bin_45, 2) bin_45,round(bin_50, 2) bin_50,round(bin_55, 2) bin_55,round(bin_60, 2) bin_60,round(bin_65, 2) bin_65,round(bin_70, 2) bin_70,round(bin_75, 2) bin_75,round(bin_80, 2) bin_80,round(bin_85, 2) bin_85,
+                                   round(bin_90, 2) bin_90,round(bin_95, 2) bin_95,round(bin_100, 2) bin_100,round(bin_105, 2) bin_105,round(bin_110, 2) bin_110,round(bin_115, 2) bin_115,round(bin_120, 2) bin_120,round(bin_125, 2) bin_125,round(bin_130, 2) bin_130,
+                                   round(bin_135, 2) bin_135,round(bin_140, 2) bin_140,round(bin_145, 2) bin_145,round(bin_150, 2) bin_150,round(bin_155, 2) bin_155,round(bin_160, 2) bin_160,round(bin_165, 2) bin_165,round(bin_170, 2) bin_170,round(bin_175, 2) bin_175,
+                                   round(bin_180, 2) bin_180,round(bin_185, 2) bin_185,round(bin_190, 2) bin_190,round(bin_195,2) bin_195, 'dead' state
+                                   FROM SCALOFF.OSDEADRES_ICE_VW  
+                                   WHERE YEAR = ", year))
+    sh.dead <-rbind(sh.dead, sh.dead.ice)
+    mw.sql.ice <- dbGetQuery(chan, paste0("SELECT CRUISE, YEAR, SURVEY_NAME, MGT_AREA_CD,  SPECIES_ID, TOW_NO, DEPTH_F, TOW_DATE, START_LAT, START_LON, END_LAT, END_LON, SCALLOP_NUM, SHELL_HEIGHT, WET_MEAT_WGT
+                              FROM SCALOFF.OSSAMPLES_ICE_VW
+                              WHERE YEAR = ", year))
+    mw.sql <-rbind(mw.sql, mw.sql.ice)
+  }
+  
+  require(lubridate)
+  sh.live$TOW_DATE <- ceiling_date(sh.live$TOW_DATE, unit = "day")
+  sh.dead$TOW_DATE <- ceiling_date(sh.dead$TOW_DATE, unit = "day")
+  mw.sql$TOW_DATE <- ceiling_date(mw.sql$TOW_DATE, unit = "day")
+  
+  sh.live$SPECIES_ID[sh.live$SPECIES_ID==1] <- "SEA SCALLOP"
+  sh.live$SPECIES_ID[sh.live$SPECIES_ID==2] <- "ICELANDIC SCALLOP"
+  sh.dead$SPECIES_ID[sh.dead$SPECIES_ID==1] <- "SEA SCALLOP"
+  sh.dead$SPECIES_ID[sh.dead$SPECIES_ID==2] <- "ICELANDIC SCALLOP"
+  mw.sql$SPECIES_ID[mw.sql$SPECIES_ID==1] <- "SEA SCALLOP"
+  mw.sql$SPECIES_ID[mw.sql$SPECIES_ID==2] <- "ICELANDIC SCALLOP"
+  
+  for (i in 1:length(banks)){
+    for (j in 1:length(cruises)){
+      if(grepl(tolower(cruises[j]), tolower(banks[i]))) cruise <- cruises[j]
+    }
+    
+    cruise <- paste0(cruise, year)
+    
+    if(banks[i] == "GB") cruise <- paste0(cruise, ".1")
+    if(banks[i] %in% c("GBa", "GBb")) cruise <- paste0("GB", year, ".2")
+    
+    if(!banks[i] %in% c("GB", "BanIce")) {
+      
+      sh.live.sub <- sh.live[sh.live$SURVEY_NAME == cruise & sh.live$MGT_AREA_CD == banks[i] & sh.live$SPECIES_ID=="SEA SCALLOP",]
+      sh.dead.sub <- sh.dead[sh.dead$SURVEY_NAME == cruise & sh.dead$MGT_AREA_CD == banks[i] & sh.dead$SPECIES_ID=="SEA SCALLOP",]
+      mw.sub <- mw.sql[mw.sql$SURVEY_NAME == cruise & mw.sql$MGT_AREA_CD == banks[i] & mw.sql$SPECIES_ID=="SEA SCALLOP",]
+      
+      sh <- dplyr::arrange(rbind(sh.live.sub, sh.dead.sub), TOW_NO)
+      mw.sub <- dplyr::arrange(mw.sub, TOW_NO, SCALLOP_NUM)
+      
+      write.csv(sh, file = paste0("Y:/Offshore scallop/Data requests/", year, "/", DR, "/", toupper(banks[i]), ".", year, ".std.hf.csv"), row.names = FALSE)
+      write.csv(mw.sub, file = paste0("Y:/Offshore scallop/Data requests/", year, "/", DR, "/", toupper(banks[i]), ".", year, ".mwsh.csv"), row.names = FALSE)
+    }
+    
+    if(banks[i] == "GB") {
+      sh.live.sub <- sh.live[sh.live$SURVEY_NAME == cruise & sh.live$MGT_AREA_CD %in% c("GBa", "GBb") & sh.live$SPECIES_ID=="SEA SCALLOP",]
+      sh.dead.sub <- sh.dead[sh.dead$SURVEY_NAME == cruise & sh.dead$MGT_AREA_CD %in% c("GBa", "GBb") & sh.dead$SPECIES_ID=="SEA SCALLOP",]
+      mw.sub <- mw.sql[mw.sql$SURVEY_NAME == cruise & mw.sql$MGT_AREA_CD %in% c("GBa", "GBb") & mw.sql$SPECIES_ID=="SEA SCALLOP",]
+      
+      sh <- dplyr::arrange(rbind(sh.live.sub, sh.dead.sub), TOW_NO)
+      mw.sub <- dplyr::arrange(mw.sub, TOW_NO, SCALLOP_NUM)
+      
+      write.csv(sh, file = paste0("Y:/Offshore scallop/Data requests/", year, "/", DR, "/", toupper(banks[i]), "spring.", year, ".std.hf.csv"), row.names = FALSE)
+      write.csv(mw.sub, file = paste0("Y:/Offshore scallop/Data requests/", year, "/", DR, "/", toupper(banks[i]), "spring.", year, ".mwsh.csv"), row.names= FALSE)
+    }
+    
+    if(banks[i]== "BanIce"){
+      sh.live.sub <- sh.live[sh.live$SURVEY_NAME == cruise & sh.live$MGT_AREA_CD == gsub(x=banks[i], "Ice", "") & sh.live$SPECIES_ID=="ICELANDIC SCALLOP",]
+      sh.dead.sub <- sh.dead[sh.dead$SURVEY_NAME == cruise & sh.dead$MGT_AREA_CD == gsub(x=banks[i], "Ice", "") & sh.dead$SPECIES_ID=="ICELANDIC SCALLOP",]
+      mw.sub <- mw.sql[mw.sql$SURVEY_NAME == cruise & mw.sql$MGT_AREA_CD == gsub(x=banks[i], "Ice", "") & mw.sql$SPECIES_ID=="ICELANDIC SCALLOP",]
+      
+      sh <- dplyr::arrange(rbind(sh.live.sub, sh.dead.sub), TOW_NO)
+      mw.sub <- dplyr::arrange(mw.sub, TOW_NO, SCALLOP_NUM)
+      
+      write.csv(sh, file = paste0("Y:/Offshore scallop/Data requests/", year, "/", DR, "/", toupper(banks[i]), ".", year, ".std.hf.csv"), row.names = FALSE)
+      write.csv(mw.sub, file = paste0("Y:/Offshore scallop/Data requests/", year, "/", DR, "/", toupper(banks[i]), ".", year, ".mwsh.csv"), row.names = FALSE)
+    }
+    
+    print(banks[i])
+    print(dim(sh))
+    print(dim(mw.sub))
+  }
+}
+
+## RUN THIS:
+detailed.tow.data(year=2019, DR="DR2019_10", banks=banks, cruises=cruises, un.ID=un.ID, pwd.ID=pwd.ID)
+
---
+++ RUNME/industry.report.R
@@ -0,0 +1,73 @@
+#### Run the industry report in R to compare against the SQL developer view
+
+source(paste(direct,"Assessment_fns/Survey_and_OSAC/get.offshore.survey.r",sep=""))
+cruise="LE10"
+yr=2019
+
+require(plyr)
+require(reshape2)
+require(dplyr)
+require(ggplot2)
+# scaloff username and pwd needed
+indreport <- get.offshore.survey(direct=direct, cruise="LE10", yr=2019, industry.report = T, un = "scaloff", pw=pwd.ID)
+
+industryreport <- indreport$industryreport
+
+# data for the summary table
+df <- NULL
+for(i in 1:length(unique(industryreport$MGT_AREA_CD))){
+  print(unique(industryreport$MGT_AREA_CD)[i])
+  
+  sub <- industryreport[industryreport$MGT_AREA_CD==unique(industryreport$MGT_AREA_CD)[i],]
+  
+  print(length(unique(sub$TOW_NO[sub$SPECIES_ID==1])))
+  print(length(unique(sub$TOW_NO[sub$SPECIES_ID==2])))
+  
+  sub$sums <- rowSums(sub[, c("L_0-70", "L_70-100", "L_100+")])
+  
+  sub$greaterthan2[sub$catchbaskets>=2] <- 1
+  sub$greaterthan200[sub$`L_100+`>=200] <- 1
+  sub$greaterthan500[sub$`L_0-70`>=500] <- 1
+
+  df[[i]] <- ddply(.data=sub, .(MGT_AREA_CD, TOW_TYPE_ID, SPECIES_ID),
+        summarize,
+        total= sum(sums),
+        total_2 = sum(greaterthan2, na.rm=T),
+        total_200 = sum(greaterthan200, na.rm=T),
+        total_500 = sum(greaterthan500, na.rm=T))
+      
+}
+
+sum(df[[1]]$total) + sum(df[[2]]$total)# + sum(df[[3]]$total)+ sum(df[[4]]$total) + sum(df[[5]]$total)  + sum(df[[6]]$total)  + sum(df[[7]]$total) 
+
+industryreport$lon <- convert.dd.dddd(industryreport$START_LON)
+industryreport$lat <- convert.dd.dddd(industryreport$START_LAT)
+ggplot() + geom_text(data=industryreport[industryreport$MGT_AREA_CD=="GBb",], aes(lon,lat, label=TOW_NO)) + coord_map()
+
+# checking out the german rake stuff. No obvious signs of bias. 
+png(paste0(direct, "2019/Presentations/Survey_summary/test_figures/Ger/numberspertow_rake.png"), height=4, width=8, units="in", res=200)
+ggplot() + geom_point(data=melt(industryreport[industryreport$MGT_AREA_CD=="Ger",], measure.vars = c("L_0-70", "L_70-100", "L_100+")), aes(TOW_NO, value)) + facet_wrap(~variable, scales="free") +
+  geom_vline(data=melt(industryreport[industryreport$MGT_AREA_CD=="Ger",], measure.vars = c("L_0-70", "L_70-100", "L_100+")), aes(xintercept=411.5), linetype="dashed") + 
+  theme_bw() + theme(panel.grid=element_blank()) +
+  ylab("Number of scallops") +
+  xlab("Tow number")
+dev.off()
+
+test <- melt(industryreport[industryreport$MGT_AREA_CD=="Ger",], measure.vars = c("L_0-70", "L_70-100", "L_100+"))
+test$rake[test$TOW_NO <= 411] <- "German rake"
+test$rake[test$TOW_NO > 411] <- "Georges rake"
+png(paste0(direct, "2019/Presentations/Survey_summary/test_figures/Ger/numberspertow_rake_spatial.png"), height=3, width=8, units="in", res=200)
+ggplot() + geom_point(data=test, 
+                      aes(convert.dd.dddd(START_LON), convert.dd.dddd(START_LAT), colour=rake, size=value)) + facet_wrap(~variable) +
+  theme_bw() + theme(panel.grid=element_blank()) +
+  ylab("Latitude") +
+  xlab("Longitude")
+dev.off()
+
+load("C:/Documents/Offshore scallop/Assessment/Data/Survey_data/2019/Survey_summary_output/testing_results_SCALOFF_LE09.RData")
+source(paste(direct,"Assessment_fns/Survey_and_OSAC/shf.plt.r",sep=""))
+shf.years <-  2018:2019
+s.size <- lined.survey.obj[[1]]$n[lined.survey.obj[[1]]$year %in% shf.years]
+shf.plt(lined.survey.obj,from='surv',yr=shf.years, col1='grey80',col2=1,rel=F,
+        recline=c(RS,CS),add.title = T,titl = NA,cex.mn=3,sample.size = T)
+           
