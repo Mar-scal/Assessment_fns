55b8772bc16649937d24737873ec7517a56b362d
Keyser <Freya.Keyser@dfo-mpo.gc.ca>
2024-06-04 09:42:08 -0300

    rgdal no longer works in R 4.4.0

--- Fishery/meat.count.table.r
+++ Fishery/meat.count.table.r
@@ -22,6 +22,8 @@ meat.count.table <- function(filenames, path)
   bank <- NULL
   # Extract the data from each word document in the list of filenames.  First we need to exclude anything that isn't a doc or txt
   fnames <- filenames[sort(c(grep(filenames, pattern=".doc"),grep(filenames, pattern=".txt")))]
+  # Exclude anything that doesn't have an underscore because sometimes processed files end up in this folder location
+  fnames <- fnames[which(grepl(fnames, pattern='_', fixed=T)==TRUE)]
   # If there happens to be any temporary files we need to toss them out the window too
   if(length(grep("~",fnames))>0) fnames <- fnames[c(-grep("~",fnames))]
   
@@ -32,18 +34,27 @@ meat.count.table <- function(filenames, path)
     # we can actually get the both to work pretty easily...
     docs <- grepl(fnames[i], pattern=".doc")
     txts <- grepl(fnames[i], pattern=".txt")
+    
     # Make sure any character strings are not being converted to factors.
     if(docs == T) txtfile[[i]] <- read.table(text=read_doc(paste0(path, fnames[i])), fill=T,stringsAsFactors = F)
     if(txts == T) txtfile[[i]] <- read.table(paste0(path, fnames[i]), fill=T,stringsAsFactors = F)
     
     # Now we need to organize and extract the data 
     bank <- paste(txtfile[[i]]$V2[1], txtfile[[i]]$V3[1], sep="") 
-    # This pulls out the meat counts, sail, land, and filename they were extracted from.  Make sure the data are not turned into factors...
+    month <- unique(month(lubridate::ymd(c(txtfile[[i]]$V5[txtfile[[i]]$V1=="Period"],
+                                    txtfile[[i]]$V7[txtfile[[i]]$V1=="Period"]))))
+    if(length(month)>1) month <- month[!is.na(month)]
+    year <- unique(year(lubridate::ymd(c(txtfile[[i]]$V5[txtfile[[i]]$V1=="Period"],
+                                           txtfile[[i]]$V7[txtfile[[i]]$V1=="Period"]))))
+    if(length(year)>1) year <- year[!is.na(year)]
+    # This pulls out the meat counts, start, end, and filename they were extracted from.  Make sure the data are not turned into factors...
     txtfile[[i]] <- data.frame(mc=txtfile[[i]]$V3[grepl(x=txtfile[[i]]$V1, pattern="Trip") & 
                                                     !is.na(as.numeric(txtfile[[i]]$V2))],
-                               sail = txtfile[[i]]$V5[txtfile[[i]]$V1=="Period"],
-                               land = txtfile[[i]]$V7[txtfile[[i]]$V1=="Period"],
-                               filename = fnames[i],stringsAsFactors =F)
+                               start = txtfile[[i]]$V5[txtfile[[i]]$V1=="Period"],
+                               end = txtfile[[i]]$V7[txtfile[[i]]$V1=="Period"],
+                               filename = fnames[i],stringsAsFactors =F,
+                               month = month,
+                               year = year)
     
     # Add the bank the the type of vessel.
     txtfile[[i]]$bank <- bank
