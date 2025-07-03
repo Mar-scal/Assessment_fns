74523b3c4692d3e92ea3abf3bca607d04b9bb9a8
Keyser <Freya.Keyser@dfo-mpo.gc.ca>
2025-07-03 12:37:14 -0300

    URL source option

--- RUNME/Loading_to_scaloff_checks.R
+++ RUNME/Loading_to_scaloff_checks.R
@@ -2,15 +2,23 @@
 #### Help is here: Y:\Offshore\Assessment\Non-Github archive and documentation\Help and Documentation/loading_to_scaloff_checks.pptx
 #### set up directory, year, cruise info...
 
+
+direct <- "Y:/Offshore/Assessment/"
 direct_fns <- "C:/Users/keyserf/Documents/Github/Assessment_fns/"
 year <- 2025
 cruise <- "LE21"
 
 #### First, check the data file-by-file (by bank) using scaloff_bank_check.R
-### Step 1: Load the scaloff_bank_check function
+### Step 1: Load the scaloff_bank_check function 
+# source locally, 
 source(paste0(direct_fns, "Survey_and_OSAC/scaloff_bank_check.R"))
-
-direct <- "Y:/Offshore/Assessment/"
+# OR FROM GITHUB:
+fun <- "https://raw.githubusercontent.com/Mar-Scal/Assessment_fns/master/Survey_and_OSAC/scaloff_bank_check.R"
+dir <- tempdir()
+temp <- dir
+download.file(fun,destfile = paste0(dir, "\\", basename(fun)))
+source(paste0(dir,"/",basename(fun)))
+file.remove(paste0(dir,"/",basename(fun)))
 
 # Make sure CSV files are saved as CSV (Comma Delimited) (*.csv) (NOT UTF-8)
 # Do not open CSV files in Excel after initial creation from XLSX file. Date format will be ruined, and you'll have to do it again.
@@ -45,11 +53,18 @@ scaloff_bank_check(direct = direct,  direct_fns=direct_fns, year = year, type = 
 ### Start by setting up some variables and loading in the function
 year <- 2025
 cruise <- "LE21"
+direct <- "Y:/Offshore/Assessment/"
 
+#source from Local:
 direct_fns <- "./"
 source(paste0(direct_fns, "Survey_and_OSAC/scaloff_cruise_check.R"))
-
-direct <- "Y:/Offshore/Assessment/"
+# OR FROM GITHUB:
+fun <- "https://raw.githubusercontent.com/Mar-Scal/Assessment_fns/master/Survey_and_OSAC/scaloff_cruise_check.R"
+dir <- tempdir()
+temp <- dir
+download.file(fun,destfile = paste0(dir, "\\", basename(fun)))
+source(paste0(dir,"/",basename(fun)))
+file.remove(paste0(dir,"/",basename(fun)))
 
 ### Step 8: Run the function with the various arguments you need:
 scaloff_cruise_check(year=year, direct=direct, direct_fns=direct_fns,
